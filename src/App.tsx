9a1e4331239f84f698392179f5b4ade9f2ad5009
pbaranchuk <pobaranchukv@gmail.com>
2023-07-24 19:42:13 +0200

    Next commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import logo from './logo512.png';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+// import Technologies from "./Technologies";
+import Abra from "./Header";
 
 
 function App() {
     return (
         <div>
-            <Header/>
-            <Technologies/>
+            {/*<Header/>*/}
+            <Abra/>
         </div>
     );
 }
+
 export default App;
+
+// Setup
+function abbrevName(name){
+    let newName = "";
+    let fName = name.split(" ")[0][0];
+    let lName = name.split(" ")[1][0];
+    newName = fName + "." + lName;
+    newName = newName.toUpperCase();
+    debugger;
+    return newName;
+}
+
+console.log(abbrevName("fYuLWGQRVH ygvLLJzO"))
--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
     return <div>
-        <a href="#s">Home</a>
-        <a href="#s">News Feed</a>
+        <a href="#s">Home</a> ----
+        <a href="#s">News Feed</a> ----
         <a href="#s">Messages</a>
     </div>
 }
