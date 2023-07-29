01701252ea66822fdc7aea357fbc9f0e8076e502
pbaranchuk <pobaranchukv@gmail.com>
2023-07-29 15:52:45 +0200

    Next commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import logo from './logo512.png';
 import './App.css';
-// import Technologies from "./Technologies";
-import Abra from "./Header";
 
 
 function App() {
     return (
         <div>
-            {/*<Header/>*/}
-            <Abra/>
+
         </div>
     );
 }
 
 export default App;
-
-// Setup
-function abbrevName(name){
-    let newName = "";
-    let fName = name.split(" ")[0][0];
-    let lName = name.split(" ")[1][0];
-    newName = fName + "." + lName;
-    newName = newName.toUpperCase();
-    debugger;
-    return newName;
-}
-
-console.log(abbrevName("fYuLWGQRVH ygvLLJzO"))
