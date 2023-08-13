569bf11207be017808431b2d2bf88927c237f665
pbaranchuk <pobaranchukv@gmail.com>
2023-08-13 11:55:41 +0200

    Next commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,34 @@ import './App.css';
 function App() {
     return (
         <div>
-
+            This is component APP
         </div>
     );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+function  Accordion(){
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
